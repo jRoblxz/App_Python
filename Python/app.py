e1bb7e8e082dcadbccd323fa8d416b102c50eac6
João Roblez <joaoroblez76@gmail.com>
2024-03-07 22:00:13 -0300

    a

--- Python/app.py
+++ Python/app.py
@@ -1,10 +1,10 @@
 import kivy
 kivy.require('2.1.0') # replace with your current kivy version !
 
-from kivy.app import App
+from kivy.App import App
 from kivy.lang import Builder
 
-GUI = Builder.load_file("tela.kv")
+GUI = Builder.load_file("style.kv")
 
 class myapp(App):
     def build(self):
