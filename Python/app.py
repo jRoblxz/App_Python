4da55e8316c5ff68cbe3fa49892a323e29ec2a48
João Roblez <joaoroblez76@gmail.com>
2024-03-07 21:54:15 -0300

    erros

--- Python/app.py
+++ Python/app.py
@@ -4,7 +4,7 @@ kivy.require('2.1.0') # replace with your current kivy version !
 from kivy.app import App
 from kivy.lang import Builder
 
-GUI = Builder.load.file("tela.kv")
+GUI = Builder.load_file("tela.kv")
 
 class myapp(App):
     def build(self):
